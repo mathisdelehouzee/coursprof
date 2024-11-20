a7a022cb4d730ecff43491114960eed6f86a9296
Mathis DELEHOUZEE <535975@umons.ac.be>
2024-11-20 10:15:36 +0100

    modif

--- src/test/java/umons/fpms/ig19/Ig19Suite.java
+++ src/test/java/umons/fpms/ig19/Ig19Suite.java
@@ -1,51 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package umons.fpms.ig19;
 
 import java.sql.Connection;
 import java.sql.Statement;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 /**
- *
- * @author noffa
+ * Suite de tests regroupant DataAccessTest, ProfTest, et CoursTest.
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({DataAccessTest.class, ProfTest.class, CoursTest.class})
 public class Ig19Suite {
 
+    /**
+     * Méthode appelée avant l'exécution de la suite de tests.
+     * Utilisée pour effectuer des configurations globales si nécessaire.
+     */
     @BeforeClass
     public static void setUpClass() throws Exception {
-        System.out.println("Suite.BeforeCLass.setUpClass");
+        System.out.println("Suite.BeforeClass.setUpClass");
+        // Ajoutez ici toute logique d'initialisation globale si besoin.
     }
 
+    /**
+     * Méthode appelée après l'exécution de la suite de tests.
+     * Utilisée ici pour supprimer le schéma utilisé par les tests.
+     */
     @AfterClass
     public static void tearDownClass() throws Exception {
         System.out.println("Suite.AfterClass.tearDownClass");
-        try (Connection con = DataAccess.connect()) {
-            try (Statement stm = con.createStatement()) {
-                if(stm.execute("DROP SCHEMA " + DataAccess.DBNAME + ";")) {
-                    System.out.println("SCHEMA '" + DataAccess.DBNAME + "' was DELETED.");
-                }
-            }
+        try (Connection con = DataAccess.connect();
+             Statement stm = con.createStatement()) {
+            // Supprime le schéma utilisé par les tests si présent.
+            stm.execute("DROP SCHEMA IF EXISTS " + DataAccess.DBNAME + ";");
+            System.out.println("SCHEMA '" + DataAccess.DBNAME + "' was DELETED.");
+        } catch (Exception e) {
+            // Capture et affiche toute erreur pendant la suppression du schéma.
+            System.err.println("Error during schema deletion: " + e.getMessage());
         }
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        System.out.println("Suite.Before.setUp");
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        System.out.println("Suite.After.tearDown");
     }
 }
